40e933b2c404022eba2326d87d04e80f66b9f0c2
Phet <rawkidz@hotmail.com>
2024-07-02 02:04:23 +0700

    [update] - add fetch api from react router

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,7 +1,7 @@
 
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-
+import { VscSearch } from 'react-icons/vsc'
 export default function SearchInput(){
     const [term, setTerm] = useState('');
     const navigate = useNavigate()
@@ -13,7 +13,18 @@ export default function SearchInput(){
 
     return (
         <form onSubmit={handleSumit}>
-            <input value={term} onChange={e => setTerm(e.target.value)} type="text" />
+            <div className="relative">
+                <div className="absolute inset-y-0 flex items-center pl-3">
+                    <VscSearch className="h-5 w-5 text-gray-500" />
+                </div>
+                <input 
+                    value={term} 
+                    onChange={e => setTerm(e.target.value)} 
+                    type="text"
+                    className="pl-10 py-2 w-full border-0 shadow-none"
+                    placeholder="Search Packages"
+                />
+            </div>
         </form>
     )
 
